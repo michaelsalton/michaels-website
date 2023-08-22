5fed6f7531728378cea1fd239c680c60d394f0bb
Michael Salton <58754252+michaelsalton@users.noreply.github.com>
2023-08-22 01:03:29 -0400

    changed site again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <main className="flex min-h-screen items-center justify-center">
-      </main>
-      <div className="white-top"></div>
-      <div className="white-bottom"></div>
-      <div className="image-placeholder">
-        <h1 className="text-4xl font-semibold">Michael Salton</h1>
+    <div className="overflow-hidden">
+      <div className="black-section h-screen">
+        {/* Content for the black section */}
+      </div>
+      <div className="white-section h-screen overflow-y-scroll">
+        {/* Content for the white section */}
+      </div>
+      <div className="black-section h-screen">
+        {/* Content for the black section */}
+      </div>
+      <div className="white-section h-screen overflow-y-scroll">
+        {/* Content for the white section */}
       </div>
     </div>
   );
